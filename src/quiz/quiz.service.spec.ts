1c4b0f1d1c3571d797ae8f8a4ca9c4ef11dc8a3d
Emirhan Tavus <emirhantavus17@gmail.com>
2025-05-24 02:38:38 +0300

    test codes add

--- src/quiz/quiz.service.spec.ts
+++ src/quiz/quiz.service.spec.ts
@@ -1,18 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { QuizService } from './quiz.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Quiz } from './quiz.entity';
 
 describe('QuizService', () => {
   let service: QuizService;
 
+  const mockQuizService = {
+    find: jest.fn(),
+    findOneBy: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [QuizService],
+      providers: [
+        QuizService,
+        {
+          provide: getRepositoryToken(Quiz),
+          useValue: mockQuizService,
+        },
+      ],
     }).compile();
 
     service = module.get<QuizService>(QuizService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should return an array o f quizzes', async () => {
+    const quizzes = [
+      { id: 1, title: 't1', description: 'd1' },
+      { id: 2, title: 't2', description: 'd2' },
+    ];
+    mockQuizService.find.mockResolvedValue(quizzes);
+
+    const result = await service.findAll();
+    expect(result).toEqual(quizzes);
   });
 });
